a44e015c92577b70d1ac83bc81e364f7e92a5911
denbmkgit <116366974+denbmkgit@users.noreply.github.com>
2024-11-23 00:10:56 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-connection =sqlite3.connect("initiate_db.db")
+connection = sqlite3.connect("initiate_db.db")
 cursor = connection.cursor()
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users(
+CREATE TABLE IF NOT EXISTS Products(
 id INTEGER PRIMARY KEY,
 title TEXT NOT NULL,
 discription TEXT,
@@ -12,20 +12,30 @@ price INTEGER NOT NULL
 )
 ''')
 
+
 def get_all_products():
-    for user in cursor.execute("SELECT * FROM Users"):
-        print(user)
+    return cursor.execute("SELECT * FROM Products")
+
+    # connection.commit()
+    # connection.close()
+
+
+# for user in get_all_products():
+#     print(user)
 
 # for i in range(1, 5):
-#     cursor.execute("INSERT INTO Users (title, discription, price) VALUES (?, ?, ?)",
+#     cursor.execute("INSERT INTO Products (title, discription, price) VALUES (?, ?, ?)",
 #                    (f"product{i}", f"product {i} is very good", f"{i}00", ))
 
-# cursor.execute("INSERT INTO Users(title, discription, price) VALUES (?, ?, ?)",
+# cursor.execute("INSERT INTO Products(title, discription, price) VALUES (?, ?, ?)",
 #                ('product4', 'product 4 is very good', 400,))
 
-# cursor.execute("UPDATE Users SET discription = ? WHERE id =?", ('product 3 is very good', '3'))
+# cursor.execute("UPDATE Products SET discription = ? WHERE id =?", ('product 3 is very good', '3'))
 
-get_all_products()
+# for i in range(5,9):
+#     cursor.execute("DELETE FROM Products WHERE id = ?", (f'{i}',))
 
-connection.commit()
-connection.close()
+# print(get_all_products())
+
+# connection.commit()
+# connection.close()
